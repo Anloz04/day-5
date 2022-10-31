50968bc5acb6cf73d90a0096d9d0f891f5230373
Anloz04 <115686548+Anloz04@users.noreply.github.com>
2022-10-31 13:59:14 +0530

    Add files via upload

---
+++ q1b.js
@@ -0,0 +1,28 @@
+// convert all the strings to title caps in a string array 
+
+// 1. Anonymous function 
+
+var a = function (str) {
+    str = str.toLowerCase().split(' ');
+
+    for (var i = 0; i < str.length; i++){
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    }
+
+    return str.join(' ');
+}
+
+console.log(a("india"));
+
+
+// 2. IIFE function 
+
+(function (str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    } 
+    return console.log(str.join(' ')); 
+  })
+
+('I LOVE INDIA');
