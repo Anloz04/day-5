14567442caa7db5f6d86cf0b96c439c9307079e2
Anloz04 <115686548+Anloz04@users.noreply.github.com>
2022-11-04 08:17:29 +0530

    Add files via upload

---
+++ q1e.js
@@ -0,0 +1,47 @@
+// Return all palindromes in array 
+
+// anonymous function 
+
+let palin = [[1,2,3], 3];
+
+function isPalindrome(N)
+    {
+        let str = "" + N;
+        let len = str.length;
+        for (let i = 0; i < parseInt(len / 2, 10); i++)
+        {
+            if (str[i] != str[len - 1 - i ])
+                return false;
+        }
+        return true;
+    }
+
+var pal = function (arr, n)
+{
+    // accessing each element of the array
+    // to check if it is palindrome or not
+    for (let i = 0; i < n; i++)
+    {
+        let ans = isPalindrome(arr[i]);
+        if (ans == false)
+            return false;
+    }
+    return true;
+}
+
+console.log(pal(palin));
+
+// IIFE function 
+
+(  function (arr, n)
+        {
+            // accessing each element of the array
+            // and to check if it is palindrome or not
+            for (let i = 0; i < n; i++)
+            {
+                let ans = isPalindrome(arr[i]);
+                if (ans == false)
+                    return false;
+            }
+            return true;
+        })([1,2,3] , 3)
